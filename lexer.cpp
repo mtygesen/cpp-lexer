9dc181a27cff11773c42ee44df3552910afa2996
Mikkel Tygesen <83021047+mtygesen@users.noreply.github.com>
2024-02-12 20:07:46 +0100

    Create lexer.cpp

---
+++ lexer.cpp
@@ -0,0 +1,94 @@
+#include "lexer.hpp"
+
+Lexer::Lexer(std::string fileName) {
+    file = std::ifstream(std::filesystem::path(fileName));
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << fileName << '\n';
+        exit(EXIT_FAILURE);
+    }
+}
+
+int Lexer::getToken() {
+    while (isspace(lastChar)) lastChar = file.get();
+
+    if (isalpha(lastChar)) {
+        identifierStr = lastChar;
+
+        while (isalnum(lastChar = file.get())) {
+            identifierStr += lastChar;
+        }
+
+        if (identifierStr == "if") {
+            return TokIf;
+        } else if (identifierStr == "else") {
+            return TokElse;
+        } else if (identifierStr == "let") {
+            return TokLet;
+        } else if (identifierStr == "for") {
+            return TokFor;
+        } else if (identifierStr == "while") {
+            return TokWhile;
+        } else if (identifierStr == "true") {
+            return TokTrue;
+        } else if (identifierStr == "false") {
+            return TokFalse;
+        } else if (identifierStr == "return") {
+            return TokReturn;
+        } else if (identifierStr == "def") {
+            return TokDef;
+        }
+
+        return TokIdentifier;
+    }
+
+    if (isdigit(lastChar) || lastChar == '.') {
+        std::string numStr;
+
+        do {
+            numStr += lastChar;
+            lastChar = file.get();
+        } while (isdigit(lastChar) || lastChar == '.');
+
+        numVal = strtod(numStr.c_str(), 0);
+
+        return TokNumber;
+    }
+
+    if (lastChar == EOF) {
+        return TokEof;
+    }   
+
+    int thisChar = lastChar;
+    lastChar = file.get();
+
+    return thisChar;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cout << "Usage: ./lexer <file>\n";  
+        return 0;
+    }
+
+    Lexer lexer(argv[1]);
+
+    int lastToken = 0;
+
+    std::string printStr;
+
+    do {
+        lastToken = lexer.getToken();
+        
+        if (lastToken < 0) {
+            printStr += lexer.tokenNames[lastToken];
+        } else {
+            printStr += lastToken;
+        }
+
+        printStr += ", ";
+    } while (lastToken != Lexer::TokEof);
+
+    printStr = printStr.substr(0, printStr.length() - 2);
+
+    std::cout << printStr << '\n';
+}
